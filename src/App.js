8fc8b988b31eae93e7584210251b64225c90f51f
Calvin Ton <cton@hawk.iit.edu>
2021-11-05 12:41:09 -0500

    added onSubmit function that prints out user input from form

--- src/App.js
+++ src/App.js
@@ -3,34 +3,22 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-datez/dist/css/react-datez.css';
 import { ReactDatez } from 'react-datez'
-import { Container, Dropdown, Form, Grid, Segment, Select, Button, Divider, Input } from 'semantic-ui-react';
+import { Container, Dropdown, Form, Grid, Segment, Select, Button, Divider, Input, Table } from 'semantic-ui-react';
 import { useState, useEffect } from 'react';
 import { restaurantOptions, transactionTimeOptions, compareType, filterOptions } from './Utility'
-
-const initialFormData = {
-  restuarantIds: [],
-  fromDate: "",
-  toDate: '',
-  fromHour: 6,
-  toHour: 29,
-  metricCriteria: [{
-    metricCode: "",
-    compareType: "",
-    value: "",
-    operatorType: 'And',
-  }]
-};
 
 function App() {
   const [ restaurantIds, setRestaurantIds ]= useState([]);
   const [ fromDate, setDateInput ] = useState("");
   const [ toDate, setToDateInput ] = useState("");
-  const [ fromHour, setFromHour ] = useState([6]);
-  const [ toHour, setToHour ] = useState([29]);
+  const [ fromHour, setFromHour ] = useState(6);
+  const [ toHour, setToHour ] = useState(29);
+  const [ metricCodeState, setMetricCode ] = useState("");
+  const [ compareTypeState, setCompareType ] = useState("");
+  const [ valueState, setValue ] = useState("");
 
-  /*
   const userPull = async () => {
-    const response = await fetch('https://http://localhost:3000/');
+    const response = await fetch('https://customsearchqueryapi.azurewebsites.net/Search/MetricDefinitions');
     const myJson = await response.json(); //extract JSON from the http response
     console.log(myJson);
   }
@@ -41,35 +29,40 @@ function App() {
 
   }, []);
 
-  const request = {"restaurantIds":[1],"fromDate":"2020-09-22T00:00:00","toDate":"2020-09-22T00:00:00","fromHour":6,"toHour":29,"metricCriteria":[{"metricCode":"TotalAmount","compareType":"GreaterThanOrEqual","value":35,"operatorType":"And"}]};
+  //const request = {"restaurantIds":[1],"fromDate":"2020-09-22T00:00:00","toDate":"2020-09-22T00:00:00","fromHour":6,"toHour":29,"metricCriteria":[{"metricCode":"TotalAmount","compareType":"GreaterThanOrEqual","value":35,"operatorType":"And"}]};
 
-  const userPush = async () => {
-    const response = await fetch('https://http://localhost:3000/', {
+  const userPush = async (formData) => {
+    const response = await fetch('https://customsearchqueryapi.azurewebsites.net/Search/Query', {
       method: 'POST',
-      body: request, // string or object
+      body: JSON.stringify(formData), // string or object
       headers: {
         'Content-Type': 'application/json'
       }
     });
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
-    console.log(response);
-  }*/
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  
-    const form = {restaurantIds, fromDate, toDate, fromHour, toHour};
-    fetch('http://http://localhost:3000/', {
-      method: 'POST',
-      body: JSON.stringify(form),
-      headers: {
-        'Content-Type': 'application/json' 
-      }
-    }).then(() => {
-      console.log(form)
-    })
+    console.log(myJson);
   }
-  
+
+  function onSubmit() {
+    const formData = {
+      restuarantIds: restaurantIds,
+      fromDate: fromDate,
+      toDate: toDate,
+      fromHour: fromHour,
+      toHour: toHour,
+      metricCriteria: [{
+        metricCode: metricCodeState,
+        compareType: compareTypeState,
+        value: parseFloat(valueState),
+        operatorType: 'And',
+      }]
+    };
+
+    console.log(formData);
+
+    userPush(formData);
+  }
 
   return (
     <div className="App">
@@ -85,7 +78,7 @@ function App() {
                 </Grid.Row>
                 <Grid.Row columns='1'>
                   <Grid.Column>
-                    <Form onSubmit={handleSubmit}>
+                    <Form onSubmit={() => onSubmit()}>
                       <Form.Field>
                         <label>Restuarant ID</label>
                         <Dropdown 
@@ -93,8 +86,7 @@ function App() {
                           multiple={true}
                           placeholder='Select Restaurant ID'
                           options={restaurantOptions}
-                          value={restaurantIds}
-                          onChange={(event, data)=> setRestaurantIds(data.value)}
+                          onChange={(event, data) => setRestaurantIds(data.value)}
                         />
                       </Form.Field>
 
@@ -107,7 +99,7 @@ function App() {
                             dateFormat='MM/DD/YYYY'
                             placeholder='MM/DD/YYYY'
                             firstDayOfWeek='Su'
-                            handleChange={value => {setDateInput(value)}}
+                            handleChange={fromDate => {setDateInput(fromDate)}}
                             value={fromDate}
                           />
                         </Form.Field>
@@ -119,7 +111,7 @@ function App() {
                             dateFormat='MM/DD/YYYY'
                             placeholder='MM/DD/YYYY'
                             firstDayOfWeek='Su'
-                            handleChange={value => {setToDateInput(value)}}
+                            handleChange={toDate => {setToDateInput(toDate)}}
                             value={toDate}
                           />
                         </Form.Field>                      
@@ -147,21 +139,26 @@ function App() {
                       <Form.Group>
                         <Form.Field
                           control={Select}
-                          label='Filter'
+                          label='Metric'
                           options={filterOptions}
-                          placeholder='Select Filter'
+                          placeholder='Select Metric'
+                          value={metricCodeState}
+                          onChange={(event, data) => setMetricCode(data.value)}
                         />
                         <Form.Field
                           control={Select}
                           options={compareType}
-                          label='Metric'
-                          placeholder='Select Metric'
+                          label='Comparator'
+                          placeholder='Select Comparator'
+                          value={compareTypeState}
+                          onChange={(event, data) => setCompareType(data.value)}
                         />
                         <Form.Field
                           control={Input}
                           label='Value'
                           placeholder='e.g. 12345'
-                          
+                          value={valueState}
+                          onChange={(event, data) => setValue(data.value)}
                         />
                       </Form.Group>
 
@@ -173,7 +170,6 @@ function App() {
                       <Form.Field>
                         <Button color='olive' type='submit'>Submit</Button>
                       </Form.Field>
-
                     </Form>
                   </Grid.Column>
                 </Grid.Row>
@@ -186,7 +182,32 @@ function App() {
           <Container>
             <Segment>
               <h3>Results</h3>
+              <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Restaurant ID</Table.HeaderCell>
+                  <Table.HeaderCell>BusDt</Table.HeaderCell>
+                  <Table.HeaderCell>Order Time</Table.HeaderCell>
+                  <Table.HeaderCell>Ticket Nunmber</Table.HeaderCell>
+                  <Table.HeaderCell>Net Amount</Table.HeaderCell>
+                  <Table.HeaderCell>Total Amount</Table.HeaderCell>
+                  <Table.HeaderCell>Items Sold</Table.HeaderCell>
+                  <Table.HeaderCell>Beverage Quantity</Table.HeaderCell>
+                  <Table.HeaderCell>Discount Amount</Table.HeaderCell>
+                  <Table.HeaderCell>Discount Ratio</Table.HeaderCell>
+                  <Table.HeaderCell>Deleted Items</Table.HeaderCell>
+                  <Table.HeaderCell>Refund Amount</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>   
+
+               <Table.Body>
+
+               </Table.Body>
+            </Table>
             </Segment>
+
+
+
           </Container>
         </Grid.Row>
       </Grid>
@@ -194,8 +215,8 @@ function App() {
   );
 }
 
-function addCriteria() {
+/*function addCriteria() {
 
-}
+}*/
 
 export default App;
